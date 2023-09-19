2f34bbd08dc9786863ca68bd6834c392b04533f3
Robinson Amianda <robinsonamianda5@gmail.com>
2023-09-18 22:42:39 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 1
+const num2 = 62
+let multiply =( num1 * num2);
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10) + 1;
+
+const num3 = 44
+const num4 = 10
+let mod = (num3% num4)
+let max = Math.max(20,19,4,5)
